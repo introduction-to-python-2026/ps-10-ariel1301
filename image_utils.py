a6a2af68c3ef1e09ee5e2dea522d5b7a57d87596
ariel1301 <ariel.ribo@gmail.com>
2026-01-02 11:07:38 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,24 +3,25 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
+    # טעינת התמונה והפיכתה למערך נומפאי
     img = Image.open(path)
     return np.array(img)
 
 def edge_detection(image):
-    if len(image.shape) == 3:
+    # המרה לגרייסקייל אם זה עדיין צבעוני
+    if image.ndim == 3:
         gray_image = np.mean(image, axis=2)
     else:
         gray_image = image
-        
-    kernelY = np.array([[ 1,  2,  1],
-                        [ 0,  0,  0],
-                        [-1, -2, -1]])
-    
-    kernelX = np.array([[-1,  0,  1],
-                        [-2,  0,  2],
-                        [-1,  0,  1]])
 
+    # פילטרים (Kernels) לזיהוי קצוות
+    kernelY = np.array([[ 1,  2,  1], [ 0,  0,  0], [-1, -2, -1]])
+    kernelX = np.array([[-1,  0,  1], [-2,  0,  2], [-1,  0,  1]])
+
+    # ביצוע הקונבולוציה
     edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill', fillvalue=0)
     edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+
+    # חישוב עוצמת הקצוות
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
     return edgeMAG
